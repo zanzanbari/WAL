faf227e2e810d695d7602b62c1b22f7c817c2015
heerucan <iwpow0307@gmail.com>
2022-04-01 13:52:35 +0900

    [Add] .gitignore 추가 (#1)

---
+++ WAL/WAL/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WAL
+//
+//  Created by Thisisme Hi on 2022/04/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
