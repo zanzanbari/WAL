39b17b075aad9872f6fdb7650852f35f950c6cc7
heerucan <iwpow0307@gmail.com>
2022-05-04 22:28:38 +0900

    [Add] Login Layout 1차 구현 (#4)

--- WAL/WAL/Network/Utility/GeneralAPI.swift
+++ WAL/WAL/Network/Utility/GeneralAPI.swift
@@ -8,5 +8,6 @@
 import Foundation
 
 struct GeneralAPI {
-    
+    static let baseURL = "https://asia-northeast3-wal-server-8a2fb.cloudfunctions.net/api/v1"
+    static let contentType = "application/json"
 }
