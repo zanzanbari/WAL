71592599438832b2df2a8a97e53a8237a854f343
heerucan <iwpow0307@gmail.com>
2022-06-14 03:35:49 +0900

    [Chore] TimeButton 재사용뷰 만들고, 초기설정뷰 3단계 셀 선택 수정 (#6)

--- WAL/WAL/Screen/Onboarding/View/AlarmCollectionViewCell.swift
+++ WAL/WAL/Screen/Onboarding/View/AlarmCollectionViewCell.swift
@@ -10,20 +10,11 @@ import UIKit
 import Then
 import WALKit
 
-//protocol TimeButtonCellSelected: AnyObject {
-//    func collectionView(collectionviewcell: TimeButtonCollectionViewCell?, index: Int, didTappedInCollectionViewCell: AlarmCollectionViewCell)
-//}
-
-class AlarmCollectionViewCell: BaseCollectionViewCell, ChangeCompleteButtonDelegate {
+class AlarmCollectionViewCell: BaseCollectionViewCell {
     
     // MARK: - Properties
     
-//    weak var timeButtonSelectedDelegate: TimeButtonCellSelected?
- 
     private let timeData = TimeData()
-        
-    // ✅ 선택된 인덱스를 알기 위한 배열
-    var selectedIndex: [Bool] = []
     
     private let titleLabel = UILabel().then {
         $0.font = WALFont.title2.font
@@ -38,22 +29,21 @@ class AlarmCollectionViewCell: BaseCollectionViewCell, ChangeCompleteButtonDeleg
         $0.numberOfLines = 0
     }
     
-    public lazy var collectionView = UICollectionView(
-        frame: .zero, collectionViewLayout: collectionViewFlowLayout).then {
-            $0.backgroundColor = .white100
-            $0.showsHorizontalScrollIndicator = false
-            $0.isScrollEnabled = true
-            $0.isUserInteractionEnabled = true
-        }
-    
-    private let collectionViewFlowLayout = UICollectionViewFlowLayout().then {
-        $0.scrollDirection = .horizontal
-    }
-    
     public let completeButton = WALPlainButton().then {
         $0.title = "완료"
         $0.isDisabled = true
     }
+    
+    private lazy var alarmButtonStackView = UIStackView().then {
+        $0.axis = .horizontal
+        $0.alignment = .fill
+        $0.distribution = .equalSpacing
+        $0.spacing = 16
+    }
+    
+    private let moringButtoon = TimeButton("아침", WALIcon.icnMorning.image)
+    private let lauchButton = TimeButton("점심", WALIcon.icnLaunch.image)
+    private let eveningButton = TimeButton("저녁", WALIcon.icnEvening.image)
     
     // MARK: - Initialize
     
@@ -61,15 +51,6 @@ class AlarmCollectionViewCell: BaseCollectionViewCell, ChangeCompleteButtonDeleg
         super.init(frame: frame)
         configUI()
         setupLayout()
-        setupCollectionView()
-        
-        // ✅ selectedIndex 배열: 카드 아이템 개수만큼 false로 초기화
-        for _ in 0..<timeData.getTimeCount() {
-            selectedIndex.append(false)
-        }
-        
-        print("---------selectedIndex: ", selectedIndex)
-
     }
     
     required init?(coder: NSCoder) {
@@ -80,13 +61,18 @@ class AlarmCollectionViewCell: BaseCollectionViewCell, ChangeCompleteButtonDeleg
     
     private func configUI() {
         contentView.backgroundColor = .white100
+        [moringButtoon, lauchButton, eveningButton].forEach {
+            $0.addTarget(self, action: #selector(touchupButton(sender:)), for: .touchUpInside)
+        }
     }
     
     private func setupLayout() {
         contentView.addSubviews([titleLabel,
                                  subtitleLabel,
-                                 collectionView,
+                                 alarmButtonStackView,
                                  completeButton])
+        
+        alarmButtonStackView.addArrangedSubviews([moringButtoon, lauchButton, eveningButton])
         
         titleLabel.snp.makeConstraints { make in
             make.top.equalToSuperview().inset(16)
@@ -97,94 +83,32 @@ class AlarmCollectionViewCell: BaseCollectionViewCell, ChangeCompleteButtonDeleg
             make.top.equalTo(titleLabel.snp.bottom).offset(9)
             make.centerX.equalToSuperview()
         }
-        
-        collectionView.snp.makeConstraints { make in
+
+        alarmButtonStackView.snp.makeConstraints { make in
             make.top.equalTo(subtitleLabel.snp.bottom).offset(35)
             make.leading.trailing.equalToSuperview().inset(20)
             make.height.equalTo(104)
         }
+        
+        [moringButtoon, lauchButton, eveningButton].forEach {
+            $0.snp.makeConstraints { make in
+            make.width.equalTo(101)
+            make.height.equalTo(104)
+        } }
         
         completeButton.snp.makeConstraints { make in
             make.leading.trailing.equalToSuperview().inset(20)
             make.bottom.equalToSuperview().inset(50)
         }
     }
-    
-    private func setupCollectionView() {
-        collectionView.delegate = self
-        collectionView.dataSource = self
-        collectionView.register(
-            TimeButtonCollectionViewCell.self,
-            forCellWithReuseIdentifier: "TimeButtonCollectionViewCell")
-    }
-        
+
     // MARK: - Custom Method
-
-    func touchupCompleteButton(isDisabled: Bool) {
-        if selectedIndex == [false, false, false] {
-            completeButton.isDisabled = isDisabled
-        } else {
-            completeButton.isDisabled = !isDisabled
-        }
-    }
-}
-
-extension AlarmCollectionViewCell: UICollectionViewDelegate {
-    func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "TimeButtonCollectionViewCell",
-                                                            for: indexPath) as? TimeButtonCollectionViewCell else { return false }
-        print("여기야 여기: ", cell.isSelected)
-        return true
-    }
-}
-
-// MARK: - UICollectionViewDataSource
-
-extension AlarmCollectionViewCell: UICollectionViewDataSource {
     
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return timeData.getTimeCount()
-    }
     
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "TimeButtonCollectionViewCell",
-            for: indexPath) as? TimeButtonCollectionViewCell
-        else { return UICollectionViewCell() }
-        cell.setupData(index: indexPath.item)
-        cell.completeButtonDelegate = self
-        // ✅ 인덱스를 cell의 indexPath로 지정
-//        cell.index = indexPath.row
-        
-        // ✅ 선택된 셀들이 어떤 건지 알기 위함
-        /*
-         나중에 선택된 셀 유형들 인덱스 번호 가지고 서버 통신 요청할 거 같은데
-         4개의 카드중에 2번째, 3번째만 선택되어있다면
-         selectedIndex 배열이 [false, true, true, false] 이렇게 있을거잖아?
-         true인 값의 인덱스만 사용하면 될 듯!
-         */
-        selectedIndex[indexPath.row].toggle()
-        print("---- 여기는 cell 내 selectedIndex", selectedIndex)
-        return cell
-    }
-}
-
-// MARK: - UICollectionViewDelegateFlowLayout
-
-extension AlarmCollectionViewCell: UICollectionViewDelegateFlowLayout {
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: (contentView.frame.width-72)/3, height: 104)
-    }
+    // MARK: - @objc
     
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return 16
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
-        return 0
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-
-        return UIEdgeInsets.zero
+    @objc func touchupButton(sender: UIButton) {
+        sender.isSelected = !sender.isSelected
+        sender.layer.borderColor = sender.isSelected ? UIColor.orange100.cgColor : UIColor.gray400.cgColor
     }
 }
---
+++ WAL/WAL/Screen/Onboarding/View/TimeButton.swift
@@ -0,0 +1,61 @@
+//
+//  TimeButton.swift
+//  WAL
+//
+//  Created by heerucan on 2022/05/07.
+//
+
+import UIKit
+
+import WALKit
+
+class TimeButton: UIButton {
+    
+    // MARK: - Property
+    
+    private let timeImageView = UIImageView()
+    
+    private let timeLabel = UILabel().then {
+        $0.font = WALFont.body6.font
+        $0.textColor = .black100
+        $0.textAlignment = .center
+    }
+    
+    // MARK: - Initialize
+    
+    init(_ text: String, _ image: UIImage) {
+        super.init(frame: .zero)
+        configUI()
+        setupLayout()
+        timeLabel.text = text
+        timeImageView.image = image
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - InitUI
+    
+    private func configUI() {
+        makeRound(radius: 10)
+        layer.borderWidth = 1
+        layer.borderColor = UIColor.gray400.cgColor
+    }
+    
+    private func setupLayout() {
+        addSubviews([timeImageView, timeLabel])
+        
+        timeImageView.snp.makeConstraints { make in
+            make.top.equalToSuperview().inset(14)
+            make.centerX.equalToSuperview()
+            make.width.height.equalTo(44)
+        }
+        
+        timeLabel.snp.makeConstraints { make in
+            make.top.equalTo(timeImageView.snp.bottom).offset(4)
+            make.centerX.equalToSuperview()
+            make.bottom.equalToSuperview().inset(20)
+        }
+    }
+}
