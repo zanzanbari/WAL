42e46753934590645eb0e19a6e10ef93011f0831
heerucan <iwpow0307@gmail.com>
2022-06-26 01:57:47 +0900

    [Chore] request body 부분 코드 변경 (#15)

--- WAL/WAL/Network/API/Onboard/OnboardAPI.swift
+++ WAL/WAL/Network/API/Onboard/OnboardAPI.swift
@@ -6,3 +6,38 @@
 //
 
 import Foundation
+
+import Moya
+
+final class OnboardAPI {
+    static let shared: OnboardAPI = OnboardAPI()
+    private let onboardProvider = MoyaProvider<OnboardService>(plugins: [MoyaLoggerPlugin()])
+    private init() { }
+    
+    public private(set) var onboardData: GenericResponse<Onboard>?
+    
+    // MARK: - POST 온보딩
+    
+    public func postOnboardSetting(nickname: String, dtype: Dtype, time: AlarmTime,
+                                completion: @escaping ((GenericResponse<Onboard>?, Int?) -> ())) {
+        
+        let param = OnboardRequest.init(nickname, dtype, time)
+        
+        onboardProvider.request(.setInfo(param: param)) { result in
+            switch result {
+            case .success(let response):
+                do {
+                    self.onboardData = try response.map(GenericResponse<Onboard>?.self)
+                    guard let onboardData = self.onboardData else { return }
+                    completion(onboardData, nil)
+                    
+                } catch(let err) {
+                    print(err.localizedDescription, 500)
+                }
+            case .failure(let err):
+                print(err.localizedDescription)
+                completion(nil, 500)
+            }
+        }
+    }
+}
--- WAL/WAL/Network/Service/Onboard/OnboardService.swift
+++ WAL/WAL/Network/Service/Onboard/OnboardService.swift
@@ -5,4 +5,31 @@
 //  Created by heerucan on 2022/06/24.
 //
 
-import Foundation
+import Moya
+
+enum OnboardService {
+    case setInfo(param: OnboardRequest)
+}
+
+extension OnboardService: BaseTargetType {
+    
+    var path: String {
+        switch self {
+        case .setInfo: return "/user/set-info"
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        default: return .post
+        }
+    }
+    
+    var task: Task {
+        switch self {
+        case .setInfo(let param):
+            return .requestJSONEncodable(param)
+        }
+    }
+}
+
