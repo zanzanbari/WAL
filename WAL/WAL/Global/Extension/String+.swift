67279ab3caa6c9c66b448b4e55db913a91291265
so yeon <alicesysy@swu.ac.kr>
2022-07-01 16:14:12 +0900

    [Add] 파일 추가 (#22)

---
+++ WAL/WAL/Global/Extension/String+.swift
@@ -0,0 +1,23 @@
+//
+//  String+.swift
+//  WAL
+//
+//  Created by heerucan on 2022/05/01.
+//
+
+import Foundation
+
+extension String {
+    func hasCharacters() -> Bool {
+        do {
+            let regex = try NSRegularExpression(pattern: "^[0-9a-zA-Z가-힣ㄱ-ㅎㅏ-ㅣ]$", options: .caseInsensitive)
+            if let _ = regex.firstMatch(in: self, options: NSRegularExpression.MatchingOptions.reportCompletion, range: NSMakeRange(0, self.count)) {
+                return true
+            }
+        } catch {
+            print(error.localizedDescription)
+            return false
+        }
+        return false
+    }
+}
