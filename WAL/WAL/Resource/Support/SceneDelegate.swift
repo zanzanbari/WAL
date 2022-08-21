3c7d1a854cce368c734ce7d4920b38c690f65e1d
so yeon <alicesysy@swu.ac.kr>
2022-08-21 16:49:29 +0900

    [Chore] 시작화면 변경 (#30)

--- WAL/WAL/Resource/Support/SceneDelegate.swift
+++ WAL/WAL/Resource/Support/SceneDelegate.swift
@@ -30,7 +30,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 //
 //        }
 //
-        window?.rootViewController = UINavigationController(rootViewController: MainViewController())
+        window?.rootViewController = LoginViewController()
         window?.makeKeyAndVisible()
     }
 
