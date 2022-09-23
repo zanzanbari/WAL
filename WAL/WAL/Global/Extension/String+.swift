033305d2db104f66274878ba74e56e29dacc5811
dlwns33 <choi990406@gmail.com>
2022-09-23 16:35:02 +0900

    [Feat] string extension에 date로 바꿔주는 함수 추가 (#57)

--- WAL/WAL/Global/Extension/String+.swift
+++ WAL/WAL/Global/Extension/String+.swift
@@ -20,4 +20,17 @@ extension String {
         }
         return false
     }
+    
+    func toDate(withFormat format: String = "yyyy. MM. dd")-> Date?{
+
+        let dateFormatter = DateFormatter()
+        dateFormatter.timeZone = TimeZone.current
+        dateFormatter.locale = Locale.current
+        dateFormatter.calendar = Calendar(identifier: .gregorian)
+        dateFormatter.dateFormat = format
+        let date = dateFormatter.date(from: self)
+
+        return date
+
+    }
 }
