5a49df31fdab9389884c868bb5592acb0ebee9e1
heerucan <iwpow0307@gmail.com>
2022-12-15 17:24:36 +0900

    [Chore] Constant로 String값 분리 (#72)

---
+++ WAL/WAL/Global/SocialType.swift
@@ -0,0 +1,8 @@
+//
+//  SocialType.swift
+//  WAL
+//
+//  Created by heerucan on 2022/12/15.
+//
+
+import Foundation
---
+++ WAL/WAL/Network/Utility/TokenManager.swift
@@ -0,0 +1,8 @@
+//
+//  TokenManager.swift
+//  WAL
+//
+//  Created by heerucan on 2022/12/13.
+//
+
+import Foundation
