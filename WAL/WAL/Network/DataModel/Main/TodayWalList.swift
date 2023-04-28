7acc850a300250a55c04fd941732472c19f76e2f
so yeon <alicesysy@swu.ac.kr>
2023-04-28 22:26:59 +0900

    [Add] Main API(v2) DTO 변경 및 enum 추가 (#96)

--- WAL/WAL/Network/DataModel/Main/TodayWalList.swift
+++ WAL/WAL/Network/DataModel/Main/TodayWalList.swift
@@ -5,24 +5,87 @@
 //  Created by 소연 on 2022/07/01.
 //
 
-import Foundation
+import UIKit
 
-/// [메인화면] 오늘의 왈소리 조회 Response
-struct TodayWalList: Codable {
-    let todayWal: [TodayWal]
-}
-
-struct TodayWal: Codable {
-    let id: Int
-    let type, content: String
-    let canOpen: Bool
-    let categoryID: Int
-    let isShown: Bool
-    let voice: String
+/// 오늘의 왈소리 목록 (메인화면 진입 시 Response)
+struct TodayWalList: BaseResponse, Codable {
+    
+    var statusCode: Int?
+    var message: String?
+    
+    let todayWalInfo: [TodayWal]?
+    
+    var statusCase: NetworkResult? {
+        return NetworkResult(rawValue: statusCode ?? -999)
+    }
     
     enum CodingKeys: String, CodingKey {
-        case id, type, content, canOpen
-        case categoryID = "categoryId"
-        case isShown, voice
+        case statusCode
+        case message
+        case todayWalInfo
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        self.statusCode = try container.decodeIfPresent(Int.self, forKey: .statusCode) ?? 0
+        self.message = try container.decodeIfPresent(String.self, forKey: .message) ?? ""
+        self.todayWalInfo = try container.decodeIfPresent([TodayWal].self, forKey: .todayWalInfo) ?? []
+    }
+    
+}
+
+/// 왈소리 1개의 데이터
+struct TodayWal: Codable {
+    let todayWalId: Int?
+    let timeType: String?
+    let categoryType: String?
+    let message: String?
+    let showStatus: String?
+    let openStatus: String?
+    
+    func getShowStatus() -> Bool {
+        return TodayWalShowStatus(rawValue: showStatus ?? "")?.status ?? false
+    }
+    
+    func getOpenStatus() -> Bool {
+        return TodayWalOpenStatus(rawValue: openStatus ?? "")?.status ?? false
+    }
+    
+    func getCategoryType() -> WalCategoryType {
+        return WalCategoryType(rawValue: categoryType ?? "") ?? .none
+    }
+    
+    func getTimeType() -> AlarmTimeType {
+        return AlarmTimeType(rawValue: timeType ?? "") ?? .none
     }
 }
+
+/// 왈소리 조회 상태 (사용자가 보았는지)
+enum TodayWalShowStatus: String {
+    case open     = "OPEN"
+    case closed   = "CLOSED"
+    
+    var status: Bool {
+        switch self {
+        case .open:      return true
+        case .closed:    return false
+        }
+    }
+}
+
+/// 왈소리 조회 가능 상태 (사용자가 볼 수 있는지)
+enum TodayWalOpenStatus: String {
+    case able     = "ABLE"
+    case disable  = "UNABLE"
+    
+    var status: Bool {
+        switch self {
+        case .able:      return true
+        case .disable:   return false
+        }
+    }
+}
+
+
+
--- WAL/WAL/Network/DataModel/Main/WalCategoryType.swift
+++ WAL/WAL/Network/DataModel/Main/WalCategoryType.swift
@@ -5,4 +5,38 @@
 //  Created by 소연 on 2023/04/27.
 //
 
-import Foundation
+import UIKit
+
+import WALKit
+
+/// 왈 타입
+enum WalCategoryType: String {
+    case none            = ""
+    case comedy          = "COMEDY"
+    case fuss            = "FUSS"
+    case comfort         = "COMFORT"
+    case yell            = "YELL"
+    case reservation     = "RESERVATION"
+    
+    var categoryId: Int {
+        switch self {
+        case .none:             return -999
+        case .comedy:           return 0
+        case .fuss:             return 1
+        case .comfort:          return 2
+        case .yell:             return 3
+        case .reservation:      return -1
+        }
+    }
+    
+    var walImage: UIImage? {
+        switch self {
+        case .none:             return nil
+        case .comedy:           return WALIcon.imgWallbbongFun.image
+        case .fuss:             return WALIcon.imgWallbbongLove.image
+        case .comfort:          return WALIcon.imgWallbbongCheer.image
+        case .yell:             return WALIcon.imgWallbbongAngry.image
+        case .reservation:      return WALIcon.imgWalbbongSpecial.image
+        }
+    }
+}
