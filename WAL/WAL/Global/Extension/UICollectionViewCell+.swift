e77ed359f133cc33840447a07a9b3c0c52776e00
so yeon <alicesysy@swu.ac.kr>
2023-04-28 22:25:06 +0900

    [Add] UICollectionViewCell extension 추가 (#96)

--- WAL/WAL/Global/Extension/UICollectionViewCell+.swift
+++ WAL/WAL/Global/Extension/UICollectionViewCell+.swift
@@ -5,4 +5,12 @@
 //  Created by 소연 on 2023/04/27.
 //
 
-import Foundation
+import UIKit
+
+extension UICollectionViewCell {
+    
+    static func register(_ target: UICollectionView) {
+        target.register(Self.self, forCellWithReuseIdentifier: Self.identifier)
+    }
+    
+}
