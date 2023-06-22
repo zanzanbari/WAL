456b39e92a6e534282c264d77085c7ba99b0d65b
heerucan <iwpow0307@gmail.com>
2023-06-22 16:45:14 +0900

    [Chore] 탈퇴 메시지 변경 (#115)

--- WAL/WAL/Global/Constant.swift
+++ WAL/WAL/Global/Constant.swift
@@ -20,7 +20,7 @@ enum Constant {
     
     enum Placeholder {
         static let warnText = "띄어쓰기 없이 한글, 영문, 숫자만 가능해요"
-        static let resignText = "탈퇴 후 탈퇴 철회(탈퇴 취소)는 불가합니다. 탈퇴 후 회원정보 및 이용 기록은 모두 삭제되며, 다시 복구할 수 없습니다. 동일한 SNS 계정을 사용한 재가입은 탈퇴한 날짜 기준 24시간 이후에 가능합니다."
+        static let resignText = "앱 사용에 불편함이 있으시다면 WAL 공식 톡 채널을 통해 알려주세요. 탈퇴 후 탈퇴 철회(탈퇴 취소)는 불가합니다. 탈퇴 후 회원정보 및 이용 기록은 모두 삭제되며, 다시 복구할 수 없습니다."
     }
         
     enum Toast {
